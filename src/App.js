4800f6e8cc2cf501ce0583d8adb6a4bd5fbd384e
Dina <hautzro@gmail.com>
2023-09-21 20:34:29 +0300

    did a bit of changing

--- src/App.js
+++ src/App.js
@@ -9,31 +9,49 @@ function App() {
   );
 }
 function Counter() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  function handleStepPlus() {
-    return setStep((s) => s + 1);
-  }
-  function handleStepMinus() {
-    return setStep((s) => s - 1);
-  }
+  const handleStepChange = (event) => {
+    const newStep = parseInt(event.target.value, 10);
+    if (!isNaN(newStep) && newStep >= 0 && newStep <= 10) {
+      setStep(newStep);
+    }
+  };
+
+  const handleCountChange = (event) => {
+    const newCount = parseInt(event.target.value, 10);
+    if (!isNaN(newCount)) {
+      setCount(newCount);
+    }
+  };
   function handleCountPlus() {
     return setCount((s) => s + step);
   }
   function handleCountMinus() {
     return setCount((s) => s - step);
   }
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
   const date = new Date("sept 17 2023");
   date.setDate(date.getDate() + count);
   return (
     <div>
-      <button onClick={handleStepMinus}>-</button>
-      <span>Step: {step}</span>
-      <button onClick={handleStepPlus}>+</button>
+      <input
+        type="range"
+        min={0}
+        max={10}
+        value={step}
+        onChange={handleStepChange}
+      />
+
+      <span>{step}</span>
+
       <div>
         <button onClick={handleCountMinus}>-</button>
-        <span>Count: {count}</span>
+        <input type="number" value={count} onChange={handleCountChange}></input>
         <button onClick={handleCountPlus}>+</button>
       </div>
       <div>
@@ -46,6 +64,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </div>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
